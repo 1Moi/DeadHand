d11b83ec424bd50a2d0ea66505ca2978d21dd32d
reza0310 <yann03duclos@gmail.com>
2025-05-14 18:30:06 +0200

    grattage en clique et auto completion

--- Assets/Scripts/ScratchTexture.cs
+++ Assets/Scripts/ScratchTexture.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ScratchTexture : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ScratchTexture : MonoBehaviour
 {
     public RectTransform rectTransform;
     public int width;
     public int height;
     public Texture2D maskTexture;
     public int scratchRadius;
-    public bool hovering;
+    private int totalPixels;
+    private int scratchedPixels;
+    private bool autoScratchTriggered = false;
+    private bool isScratchDisabled = false;
+    private Coroutine autoScratchCoroutine;
 
     private void Start()
     {
@@ -24,10 +28,21 @@ public class ScratchTexture : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         rectTransform = GetComponent<RectTransform>();
         width = (int)rectTransform.sizeDelta.x;
         height = (int)rectTransform.sizeDelta.y;
-        maskTexture = new Texture2D(width, height);
-        GetComponent<Image>().material.mainTexture = maskTexture;
+        maskTexture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+        Color32[] fillColor = new Color32[width * height];
+        for (int i = 0; i < fillColor.Length; i++)
+            fillColor[i] = new Color32(0, 0, 0, 255);  // opaque black
+
+        maskTexture.SetPixels32(fillColor);
         maskTexture.Apply(false);
+
+        GetComponent<Image>().material.mainTexture = maskTexture;
+
+        totalPixels = width * height;
+        scratchedPixels = 0;
     }
+
 
     public void ScratchAccordingToMousePosition(int xVal, int yVal)
     {
@@ -63,29 +78,113 @@ public class ScratchTexture : MonoBehaviour, IPointerEnterHandler, IPointerExitH
             if (pixels[index].a != 0)
             {
                 pixels[index].a = 0;
+                scratchedPixels++;
                 return true;
             }
         }
         return false;
     }
 
+
     private void Update()
     {
-        if (hovering)
+        if (Input.GetMouseButton(0)) // Clic gauche maintenu
         {
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, Camera.main, out Vector2 localPos);
-            ScratchAccordingToMousePosition((int)localPos.x, (int)localPos.y);
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, Camera.main, out Vector2 localPos))
+            {
+                ScratchAccordingToMousePosition((int)localPos.x, (int)localPos.y);
+            }
+        }
+
+        // Vérifie le pourcentage gratté
+        if (!autoScratchTriggered)
+        {
+            float scratchedPercentage = (float)scratchedPixels / totalPixels;
+
+            if (scratchedPercentage >= 0.45f)
+            {
+                autoScratchTriggered = true;
+                AutoScratchRemaining();
+            }
         }
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+
+    void AutoScratchRemaining()
     {
-        hovering = true;
-        Debug.Log("Hoverin : " + hovering);
+        autoScratchCoroutine = StartCoroutine(ScratchFromCenterOutward());
+        StartCoroutine(StopAutoScratchAfterDelay(2f));
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    IEnumerator StopAutoScratchAfterDelay(float delay)
     {
-        hovering = false;
+        yield return new WaitForSeconds(delay);
+
+        if (autoScratchCoroutine != null)
+        {
+            StopCoroutine(autoScratchCoroutine);
+            autoScratchCoroutine = null;
+        }
+
+        ScratchAllRemaining(); // Grattage immédiat du reste
     }
+
+
+    IEnumerator ScratchFromCenterOutward()
+    {
+        isScratchDisabled = true;
+
+        Color32[] pixels = maskTexture.GetPixels32();
+        Vector2 center = new Vector2(width / 2, height / 2);
+        int maxRadius = Mathf.CeilToInt(Mathf.Sqrt(width * width + height * height));
+
+        for (int r = 0; r <= maxRadius; r += 10) 
+        {
+            // Dessine un disque de rayon `r`
+            for (int dx = -r; dx <= r; dx++)
+            {
+                int dyLimit = Mathf.FloorToInt(Mathf.Sqrt(r * r - dx * dx));
+                for (int dy = -dyLimit; dy <= dyLimit; dy++)
+                {
+                    int x = Mathf.RoundToInt(center.x + dx);
+                    int y = Mathf.RoundToInt(center.y + dy);
+
+                    if (x >= 0 && x < width && y >= 0 && y < height)
+                    {
+                        int index = y * width + x;
+                        if (pixels[index].a != 0)
+                        {
+                            pixels[index].a = 0;
+                        }
+                    }
+                }
+            }
+
+            maskTexture.SetPixels32(pixels);
+            maskTexture.Apply();
+
+            yield return null; // pause visuelle
+        }
+
+        Debug.Log("Auto-scratch complet !");
+    }
+
+    void ScratchAllRemaining()
+    {
+        Color32[] pixels = maskTexture.GetPixels32();
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            if (pixels[i].a != 0)
+            {
+                pixels[i].a = 0;
+            }
+        }
+
+        maskTexture.SetPixels32(pixels);
+        maskTexture.Apply();
+
+        Debug.Log("Grattage complet effectué !");
+    }
+
 }
