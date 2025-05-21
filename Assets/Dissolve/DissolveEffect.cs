9f5d3407b8042fa61801e5a9708a0397beb85e71
MathisLISAA <122012065+MathisLISAA@users.noreply.github.com>
2025-05-21 15:17:12 +0200

    Cartes de fin - DissolveEffect

---
+++ Assets/Dissolve/DissolveEffect.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    private float DissolveAmount;
+    private bool IsDissolving;
+
+    private void Update()
+    {
+        if (IsDissolving)
+        {
+            DissolveAmount = Mathf.Clamp01(DissolveAmount + Time.deltaTime);
+            material.SetFloat("_DissolveAmount", DissolveAmount);
+        }
+
+        else
+        {
+            DissolveAmount = Mathf.Clamp01(DissolveAmount - Time.deltaTime);
+            material.SetFloat("_DissolveAmount", DissolveAmount);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+                IsDissolving = true;
+                UnityEngine.Debug.Log("DIssolve COMMENCE");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            IsDissolving = false;
+            UnityEngine.Debug.Log("DIssolve RETREAT");
+        }
+
+    }
+}
